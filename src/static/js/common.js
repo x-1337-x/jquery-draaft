c2df4715e6440592cc4d4a7f7c6480d1b1376eda
Joe Bazooka <Joe Bazooka>
2016-08-08 16:43:21 +0300

    forms done

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,1 +1,42 @@
-console.log('i am common.js');
+console.log('common.js says \'Hello\'.');
+
+
+var modal = document.getElementsByClassName('l-modalRegister')[0];
+var btn = document.getElementById("openModal");
+var btnCloseModal = document.getElementById("modalBtnClose");
+var modalForm = document.getElementById("modalRegisterForm");
+var modalSuccess = document.getElementById("modalRegisterSuccess");
+var close = document.getElementsByClassName("c-close-modal")[0];
+
+
+
+//For example display purposes, to be deleted========
+
+var showSuccess = document.getElementById("successExample");
+
+showSuccess.onclick = function() {
+  modalForm.style.display = "none";
+  modalSuccess.style.display = "block";
+}
+
+//===================================================
+
+
+
+btnCloseModal.onclick = function() {
+  modal.style.display = "none";
+}
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+close.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
